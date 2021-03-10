35da5597954dfc9a3c38fc8bc332e3d70fc379e9
Nima Soufiloo <nima.soufiloo@gmail.com>
2021-03-10 19:35:56 +0330

    added default export in index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * from "./file.module";
 export * from "./file.service";
+export * from "./types/minio.config.interface";
+export * from "./types/file.model";
+export * from "./types/response.dto";
